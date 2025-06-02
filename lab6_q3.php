fef151ab7e49dc69e4df2d665333c98c3e50e9ce
Qamarina Hanani <hananiiajmain14@gmail.com>
2025-06-02 15:55:22 +0800

    edit code

--- lab6_q3.php
+++ lab6_q3.php
@@ -1,14 +1,12 @@
 <?php
-// Define the function to calculate the area
+
 function calculateArea($width, $length) {
     return $width * $length;
 }
 
-// Example values
 $width = 4;
 $length = 2;
 
-// Call the function and store the result
 $area = calculateArea($width, $length);
 ?>
 
